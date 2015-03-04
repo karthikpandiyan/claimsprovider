62eccee2e17be5361d6586435a23bad7eeede98a
karthikpandiyan <karthikpandiyan@hotmail.com>
2015-03-04 15:33:06 +0530

    6

--- SharePointClaimsWeb/Pages/Default.aspx.cs
+++ SharePointClaimsWeb/Pages/Default.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.SharePoint.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -30,14 +31,43 @@ namespace SharePointClaimsWeb
         {
             // The following code gets the client context and Title property by using TokenHelper.
             // To access other properties, the app may need to request permissions on the host web.
-            var spContext = SharePointContextProvider.Current.GetSharePointContext(Context);
+            //var spContext = SharePointContextProvider.Current.GetSharePointContext(Context);
 
-            using (var clientContext = spContext.CreateUserClientContextForSPHost())
+            //using (var clientContext = spContext.CreateUserClientContextForSPHost())
+            //{
+            //    clientContext.Load(clientContext.Web, web => web.Title);
+            //    clientContext.ExecuteQuery();
+            //    Response.Write(clientContext.Web.Title);
+            //}
+
+
+            using (var clientContext = this.GetClientContextWithAccessToken("https://jcistage.sharepoint.com/"))
             {
                 clientContext.Load(clientContext.Web, web => web.Title);
                 clientContext.ExecuteQuery();
                 Response.Write(clientContext.Web.Title);
             }
         }
+
+        /// <summary>
+        /// gets access token and returns clientcontext
+        /// </summary>
+        /// <returns></returns>
+        public ClientContext GetClientContextWithAccessToken(string url)
+        {
+            Uri siteUri = new Uri(url);
+            string realm = TokenHelper.GetRealmFromTargetUrl(siteUri);
+
+            //Get the access token for the URL.  
+            string accessToken = TokenHelper.GetAppOnlyAccessToken(
+                TokenHelper.SharePointPrincipal,
+                siteUri.Authority, realm).AccessToken;
+
+            //Get client context with access token
+            ClientContext clientcontext = TokenHelper.GetClientContextWithAccessToken(siteUri.ToString(), accessToken);
+            return clientcontext;
+        }
+
+
     }
 }
